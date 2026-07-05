cf502043da0fb12cf55efbcb23ed725578e9689d
Miro1380 <miro1380@gmail.com>
2026-07-05 00:11:43 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+
+        for(String str : strs){
+            Integer length = str.length();
+            String encode = length + "#"+ str;
+            //System.out.println("Encoding: "+ encode);
+            encodedString.append(encode);
+        }
+
+        //System.out.println("Encoded String: "+ encodedString);
+        return encodedString.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> decodedList= new ArrayList<>();
+        int i = 0;
+        int j = 1;
+        
+        //System.out.println("j: "+ j);
+        //System.out.println("TEST BEFORE str.length(): "+ str.length());
+
+        while(j < str.length()){
+
+            //If j finds a # take a substring from i to j?
+            //The word starts at j+1 and ends at j+1 + length (number before #)
+            //Take substring, move i to end value (j+1+length) and move j to +1 of that?
+            //System.out.println("CHAR: "+ str.charAt(j));
+            if(str.charAt(j) == '#'){
+                Integer length = Integer.parseInt(str.substring(i,j));
+                Integer begin = j+1;
+                Integer end = j+1+length;
+                String decode = str.substring(begin, end);
+                decodedList.add(decode);
+                i = end;
+                j = end;
+                continue;
+            }
+            j++;
+        }
+        return decodedList; 
+    }
+}
